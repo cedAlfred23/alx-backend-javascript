da9093870946e953e2eec687689559518ff73835
cedAlfred23 <al.cedricgbaguidi@gmail.com>
2024-04-26 18:46:50 -0700

    1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,11 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(24)];
+  const sizes = [19, 20, 34];
+  const classArray = [];
+
+  for (const size of sizes) {
+    classArray.push(new ClassRoom(size));
+  }
+  return classArray;
 }
