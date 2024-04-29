6071efc1ac31fb243f57d1c73d768e056a1dcd23
cedAlfred23 <al.cedricgbaguidi@gmail.com>
2024-04-29 12:02:29 -0700

    adding all files

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,11 @@
-export function getListStudents(){
-	const response = [
+const getListStudents = () => {
+  const students = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
-	return reponse;
-}
+
+  return students;
+};
+
+export default getListStudents;
